0826f74e658a64ac4a8f5d313ec3c7ce84eb0085
David Kalajdzic <david.kalajdzic@icloud.com>
2023-02-23 21:39:22 +0100

    for testing

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText t = findViewById(R.id.mainName);
+        Button b = findViewById(R.id.mainGoButton);
+        b.setOnClickListener(v -> {
+            Intent myIntent = new Intent(getApplicationContext(), GreetingActivity.class);
+            String msg = "Hello " + t.getText().toString() + "!";
+            myIntent.putExtra("key", msg);
+            MainActivity.this.startActivity(myIntent);
+        });
+    }
+}
